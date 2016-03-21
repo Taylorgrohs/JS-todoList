ebefc640226f6c6da2173e984f3de51a974875c2
Nathan Otto and Taylor Grohs <nathan@>
2016-03-21 11:38:58 -0700

    add todo

---
+++ js/browser-interface.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  tasks.forEach(function(i){
+    $('#task').append("<li>" + i.constructor.name + "</ul><ul><li>" + i.description + "<ul><li> Priority: " + i.priority +"</li></ul></li>");
+  });
+})
